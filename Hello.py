8fa054c4bae33b74c37e86f22d99bec039e15d8b
Ryan Fuller <rfuller250@yahoo.com>
2016-08-26 12:10:30 -0500

    Finished Hello.py

---
+++ Hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# --------
+# Hello.py
+# --------
+
+# https://www.python.org
+# https://docs.python.org/3.5/
+# https://docs.python.org/3.5/library/
+
+import sys
+
+print(sys.version)
+print("Hello, World!")
